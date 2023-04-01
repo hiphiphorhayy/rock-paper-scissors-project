7a2170faef4b86a2d457ad72228e0461b966d2c1
George Montoya <94158336+hiphiphorhayy@users.noreply.github.com>
2023-03-31 23:46:25 -0400

    Create functions for computer choice & game round

--- index.js
+++ index.js
@@ -13,4 +13,35 @@ for (let choice of answers) {                                //to check users ch
         break;
     }
 }
+                            //function that randomly returns choice for computer
+let getComputerChoice = () => answers[Math.floor(Math.random() * answers.length)];
 
+function round (user) {           //function for each round
+    let computer = getComputerChoice();
+    if (user === computer) {
+        return "It's a tie!"
+    }
+    else if (user === r && computer === s) {
+        return `You win! ${r} beats ${s}`
+    }
+    else if (user === p && computer === r) {
+        return `You win! ${p} beats ${r}`
+    }
+    else if (user === s && computer === p) {
+        return `You win! ${s} beats ${p}`
+    }
+                                            //^ tie and winning logic in if statements
+                                            //V losing logic in if statements
+    else if (user === r && computer === p) {
+        return `You lose! ${r} beats ${p}`
+    }
+    else if (user === p && computer === s) {
+        return `You lose! ${p} beats ${s}`
+    }
+    else if (user === s && computer === r) {
+        return `You lose! ${s} beats ${r}`
+    }
+}
+
+let result = round(playerSelection);
+console.log(result);
