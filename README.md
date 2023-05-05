753a816cedbb8d55d4f98d86b655e171b1e0069d
George Montoya <94158336+hiphiphorhayy@users.noreply.github.com>
2023-05-05 00:46:27 -0400

    Add UI to game

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # rock-paper-scissors-project
 
 ## A simple rock paper scissors console game
-Game to be played between user and computer
+## Game to be played between user and computer
+## Recently added a UI to the game
--- index.js
+++ index.js
@@ -5,64 +5,100 @@ const answers = [r, p, s]       //make an array for the choices and iterate thro
 
 let getComputerChoice = () => answers[Math.floor(Math.random() * answers.length)];
                                     //^function that randomly returns choice for computer
-function round (user) {           //function for each round
+
+let scoreMessage = document.querySelector('.score');
+let roundMessage = document.querySelector('.round');
+
+let newRound = document.createElement('p');
+let newScore = document.createElement('p');
+newScore.textContent = 'Press a button to play';
+
+
+scoreMessage.appendChild(newScore);
+roundMessage.appendChild(newRound);
+
+function round(user) {           //function for each round
     let computer = getComputerChoice();
     if (user === computer) {
-        return "It's a tie!"
+        newRound.textContent = "It's a tie!";
+        return newRound.textContent;
     }
     else if (user === r && computer === s) {
-        return `You win! ${r} beats ${s}`
+        newRound.textContent = `You win! ${r} beats ${s}`;
+        return newRound.textContent;
     }
     else if (user === p && computer === r) {
-        return `You win! ${p} beats ${r}`
+        newRound.textContent = `You win! ${p} beats ${r}`;
+        return newRound.textContent;
     }
     else if (user === s && computer === p) {
-        return `You win! ${s} beats ${p}`
+        newRound.textContent = `You win! ${s} beats ${p}`;
+        return newRound.textContent;
     }
                                             //^ tie and winning logic in if statements
                                             //V losing logic in if statements
     else if (user === r && computer === p) {
-        return `You lose! ${p} beats ${r}`
+        newRound.textContent = `You lose! ${p} beats ${r}`;
+        return newRound.textContent;
     }
     else if (user === p && computer === s) {
-        return `You lose! ${s} beats ${p}`
+        newRound.textContent = `You lose! ${s} beats ${p}`;
+        return newRound.textContent;
     }
     else if (user === s && computer === r) {
-        return `You lose! ${r} beats ${s}`
+        newRound.textContent = `You lose! ${r} beats ${s}`;
+        return newRound.textContent;
     }
 }
 
-function game () {
-    let winsLossesTies = [0,0,0]    //make an array to keep track of wins losses and ties
-    
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock, paper, or scissors?', '');  //prompt user for choice
-        while (!answers.includes(playerSelection.toLowerCase())) { //while loop of input lowercased until valid response is given
-            playerSelection = prompt('Invalid response! Choose rock, paper, or scissors.', '').toLowerCase();
-        }               //repeatedly prompt user and lowercase the response
-        
-        playerSelection = playerSelection.toLowerCase();        //save lowercase version of input to playerSelection variable
+let buttons = document.querySelectorAll('button');
+let winsLossesTies = [0,0,0]    //make an array to keep track of wins losses and ties
 
-        let result = round(playerSelection);        //call round function and console log the result
-        console.log(result);
+function playRound(button) {
+    let result = round(button.textContent.toLowerCase());
+    newRound.textContent = result;
 
-        if (result.startsWith('You win')) {     //if else statement to track results with array of wins losses and ties
-            winsLossesTies[0]++;
-        } else if (result.startsWith('You lose')) {
-            winsLossesTies[1]++;
-        } else if (result.startsWith('It\'s a')) {
-            winsLossesTies[2]++;
+    if (result.startsWith('You win')) {     //if else statement to track results with array of wins losses and ties
+        winsLossesTies[0]++;
+    } else if (result.startsWith('You lose')) {
+        winsLossesTies[1]++;
+    } else if (result.startsWith("It's a")) {
+        winsLossesTies[2]++;
+    }
+
+    newScore.textContent = `${winsLossesTies[0]} wins, ${winsLossesTies[1]} losses, ${winsLossesTies[2]} ties`;
+
+    for (let outcome of winsLossesTies) {
+        if (outcome == 5) {
+            finalResponse();
+            break;
+        } else {
+            continue;
         }
     }
-
-    console.log(`${winsLossesTies[0]} wins, ${winsLossesTies[1]} losses, ${winsLossesTies[2]} ties`);
-    if (winsLossesTies[0] > winsLossesTies[1]) {        //final response giving total # of wins losses and ties and declaring final result of game
-        console.log('You win the game!');
-    } else if (winsLossesTies[0] < winsLossesTies[1]) {
-        console.log('You lost the game..');
-    } else {
-        console.log('Game tie?! You should play again');
-    }
 }
 
-game();
+buttons.forEach(button => button.addEventListener('click', () => playRound(button)));
+
+function finalResponse() {
+    if (winsLossesTies[0] > winsLossesTies[1] && winsLossesTies[0] > winsLossesTies[2]) {        //final response giving total # of wins losses and ties and declaring final result of game
+    newScore.textContent = 'You win the game!';
+    } else if (winsLossesTies[1] > winsLossesTies[0] && winsLossesTies[1] > winsLossesTies[2]) {
+    newScore.textContent = 'You lost the game..';
+    } else if (winsLossesTies[2] > winsLossesTies[0] && winsLossesTies[2] > winsLossesTies[1]) {
+    newScore.textContent = 'Game tie?! You should play again';
+    }
+
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Play again?';
+    newRound.textContent = '';
+    roundMessage.appendChild(resetButton);
+    buttons.forEach(button => button.disabled = true);
+
+    resetButton.addEventListener('click', () => {
+        winsLossesTies = [0,0,0];
+        newScore.textContent = '';
+        roundMessage.removeChild(resetButton);
+        buttons.forEach(button => button.disabled = false);
+    });
+}
